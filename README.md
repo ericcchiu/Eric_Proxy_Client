b8f3bea0453155eb9cf5a51d464a645f3e401aa9
ericcchiu <eric.chn.chiu@gmail.com>
2018-12-19 16:01:29 -0600

    (setup) Initialize and refactor Proxy client.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Development of a Proxy-Service
-Includes: Frontend and Backend. With a proxy server that routes to multiple services. 
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
+
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`);
+  console.log(`Eric Proxy Server running at: http://localhost:${port}`);
 });
