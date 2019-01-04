15a49d6dbcd91128fa3f56420e1684bebde71e8a
ericcchiu <eric.chn.chiu@gmail.com>
2019-01-03 19:10:27 -0600

    (implementation) Add related projects services to proxy server. Update package json. Restructure folder structure to ensure master copy uniformity.

--- client/ProxyClient.js
+++ client/ProxyClient.js
@@ -1,16 +1,39 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.changeProject = this.changeProject.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      id: Math.floor(Math.random() * 99 + 1)
+    };
+  }
+
+  changeProject() {
+    this.setState({
+      id: Math.floor(Math.random() * 99 + 1)
+    });
+  }
+
+  handleClick(event) {
+    this.setState({
+      id: event.target.id
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Welcome to Eric's Proxy Client</h1>
-        <script src="http://localhost:3002/bundle.js"></script>
+        <Project id={this.state.id} />
+        <Related id={this.state.id} />
+        <button onClick={this.changeProject} value="Change Project" />
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById("root"));
--- server.js
+++ server.js
@@ -1,16 +1,41 @@
-
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
-const app = express();
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const axios = require("axios");
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public/dist')));
+const app = express();
 
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname +'/public/dist/index.html'); // change the path to your index.html
-// });
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public/dist")));
+
+app.get("/projects", (req, res) => {
+  axios
+    .get("http://localhost:3002/")
+    .then(list => {
+      res.status(200);
+      res.json(list.data);
+    })
+    .catch(err => {
+      console.log("Error with GET request to Projects Services from Proxy");
+      res.sendStatus(500);
+    });
+});
+
+app.get("/related", (req, res) => {
+  axios
+    .get("http://localhost:3004/")
+    .then(list => {
+      res.status(200);
+      res.json(list.data);
+    })
+    .catch(err => {
+      console.log("Error with GET request to Projects Services from Proxy");
+      res.sendStatus(500);
+    });
+});
 
 app.listen(port, () => {
   console.log(`Eric Proxy Server running at: http://localhost:${port}`);
