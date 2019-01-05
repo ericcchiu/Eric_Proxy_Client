232aa1c677affea4d68aef366f2560ee0e25b1fb
ericcchiu <eric.chn.chiu@gmail.com>
2019-01-05 11:31:45 -0600

    (bug-fix) Fix path to reflect correct path on github.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       host: "ec2-18-223-115-68.us-east-2.compute.amazonaws.com",
       key: "~/.ssh/projectView.pem",
       ref: "origin/master",
-      repo: "git@github.com:ericcchiu/Eric_Services_Projects.git",
+      repo: "git@github.com:ericcchiu/Eric_Proxy_Client.git",
       path: "/home/ubuntu/proxyClient",
       "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js"
     }
